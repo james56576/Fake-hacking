dab4d1796c0f8277140456715bc8f1510e14c524
james56576 <101009316+james56576@users.noreply.github.com>
2023-01-30 07:10:45 +0000

    v1
    added menu that dose nothing
    
    first version

---
+++ assets/main.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title Fake hacking
+color 07
+
+:menu
+cls
+echo ---------------------------------------
+echo.
+echo.
+echo Please select a fake hack to run.
+echo.
+echo 1) eeee
+echo 2) eeee
+echo 3) eeee
+echo 4) eeee
+echo.
+echo ---------------------------------------
+
+set /p choice="Enter a number: "
+
+if %errorlevel% == 1 goto hack0
+if %errorlevel% == 2 goto hack1
+if %errorlevel% == 3 goto hack2
+
+:: work on this later gtg to school aka hell
